337405522c17fd188db15366ab3fe6d8cc2b9d4d
Marian Busila <marian.busila@gmail.com>
2018-12-29 22:39:48 -0500

    add persistence to mongo db

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask, redirect, url_for
+import os
+
+from flask import Flask, redirect, url_for, request, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient(os.environ['MONGODB_HOST'], 27017)
+db = client.tododb
 
 @app.route('/')
 def todo():
-    return "Hello!"
+    _items = db.tododb.find()
+    items = [item for item in _items]
+    return render_template('todo.html', items = items)
 
 @app.route('/new', methods=['POST'])
 def new():
+    item_doc = {
+        'name':  request.form['name'],
+        'description':  request.form['description']
+    }
+
+    db.tododb.insert_one(item_doc)
+
     return redirect(url_for('todo'))
 
 if __name__ == "__main__":
